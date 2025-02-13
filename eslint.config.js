76214013811482de4def7506dd3caf7f7820513b
JKapturkiewicz <kuba.kapek@gmail.com>
2025-02-13 16:16:47 +0100

    chore: update eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -25,6 +25,12 @@ export default tseslint.config(
 		}
 	},
 	{
+		rules: {
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': 'warn'
+		}
+	},
+	{
 		ignores: ['build/', 'dist/']
 	}
 )
