3122e77995aff7295f37edaf27f90a23a704d506
JKapturkiewicz <kuba.kapek@gmail.com>
2025-02-13 16:54:02 +0100

    chore: include etherum in the Window interface

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+declare interface Window {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	ethereum?: any
+}
