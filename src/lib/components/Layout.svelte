67b2ccbea459a9dbe6657ae67f712aca9432cae6
JKapturkiewicz <kuba.kapek@gmail.com>
2025-02-13 18:15:45 +0100

    chore: create Layout component

---
+++ src/lib/components/Layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { type Snippet } from "svelte"
+	import Header from "./Header.svelte"
+	import ToastContainer from "./ToastContainer.svelte"
+
+	type Props = {
+		children: Snippet
+	}
+	let { children }: Props = $props()
+</script>
+
+<div class="layout">
+	<Header />
+	<main>
+		{@render children()}
+	</main>
+	<footer></footer>
+</div>
+
+<ToastContainer />
+
+<style>
+	.layout {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+		width: 100%;
+		max-width: 1024px;
+		margin: auto;
+	}
+	main {
+		padding: 1rem;
+		display: flex;
+		flex-direction: column;
+		place-items: center;
+		gap: 1rem;
+	}
+</style>
