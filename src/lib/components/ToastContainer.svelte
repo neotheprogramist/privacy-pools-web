5e8491a00416c1cad00c3bd62c75f3e1d2362a48
JKapturkiewicz <kuba.kapek@gmail.com>
2025-02-13 17:08:59 +0100

    chore: implement toast notifications

---
+++ src/lib/components/ToastContainer.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { toasts } from "../stores/toastStore"
+	import Toast from "./Toast.svelte"
+</script>
+
+<div class="container">
+	{#each $toasts as toast (toast.id)}
+		<Toast
+			message={toast.message}
+			type={toast.type}
+			duration={toast.duration}
+			onRemove={() => toasts.remove(toast.id)}
+		/>
+	{/each}
+</div>
+
+<style>
+	.container {
+		position: fixed;
+		bottom: 1rem;
+		left: 1rem;
+		z-index: 1000;
+	}
+</style>
